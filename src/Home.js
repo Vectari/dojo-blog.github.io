1f4182508cf771194ffa3009ce052e32c0b154d0
Vectari <mateusz.majer996@gmail.com>
2022-10-19 07:25:16 +0200

    Add json, loading message,

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() => {
+       setTimeout(() => {                       // USE SetTimeout only to simulate Loading screen
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+        });
+       }, 1000);
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!"/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>           
+            { isPending && <div>Loading... </div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
 }
 
-export default Home;
+export default Home;    
